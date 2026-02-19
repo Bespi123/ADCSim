5112a484d67548f1a11f71c249ff246ba9a7f37f
Brayan espinoza <b_antonioesp@hotmail.com>
2026-02-18 21:14:29 -0500

    Fix bugs and organize code

--- src/+adcsim/+utils/calculateSettlementTime.m
+++ src/+adcsim/+utils/calculateSettlementTime.m
@@ -1,17 +1,17 @@
 function ts = calculateSettlementTime(e, t, tol)
-    % calculateSettlementTime computes the settlement time for a given signal `e`.
-    % The function checks when the signal `e` first enters and stays within the defined 
-    % tolerance range for the entire signal and returns the corresponding time at that 
-    % point. If the signal never stays within the range, it returns NaN.
-    %
-    % Inputs:
-    %   e   - A vector representing the signal values (e.g., angular error or any monitored variable)
-    %   t   - A vector representing the time instances corresponding to each value in `e`.
-    %   tol - A scalar tolerance value defining the acceptable range for the signal `e`.
-    %
-    % Output:
-    %   ts  - The settlement time, i.e., the time at which the signal `e` stays within 
-    %         the tolerance range [-tol, tol]. If the signal never settles, `ts` will be NaN.
+% calculateSettlementTime computes the settlement time for a given signal `e`.
+% The function checks when the signal `e` first enters and stays within the defined 
+% tolerance range for the entire signal and returns the corresponding time at that 
+% point. If the signal never stays within the range, it returns NaN.
+%
+% Inputs:
+%   e   - A vector representing the signal values (e.g., angular error or any monitored variable)
+%   t   - A vector representing the time instances corresponding to each value in `e`.
+%   tol - A scalar tolerance value defining the acceptable range for the signal `e`.
+%
+% Output:
+%   ts  - The settlement time, i.e., the time at which the signal `e` stays within 
+%         the tolerance range [-tol, tol]. If the signal never settles, `ts` will be NaN.
     
     %%% Set bounds based on the given tolerance (lower and upper bounds)
     bounds = [-tol, tol];  % The bounds are set as [-tol, tol]
