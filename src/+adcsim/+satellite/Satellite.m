2a52e782edb3ccef9ef6a25c87c4b210a2f33778
Brayan espinoza <b_antonioesp@hotmail.com>
2026-03-05 00:19:15 -0500

    Update mathematical model to get magnetic field

--- src/+adcsim/+satellite/Satellite.m
+++ src/+adcsim/+satellite/Satellite.m
@@ -47,32 +47,43 @@ classdef Satellite
         end
         
         % --- MAIN PUBLIC METHOD ---
-        function obj = updateState(obj, T_control, T_disturbance, dt)
-            % updateState Integrates the satellite's dynamics over a single time step.
+        function obj = updateState(obj, T_ext, hw, hw_dot, dt)
+            % UPDATESTATE Integrates the satellite's dynamics over a single time step.
             %
             %   This method uses a 4th-order Runge-Kutta (RK4) integrator to
-            %   propagate the satellite's state. This is the primary public method
-            %   that should be called from the main simulation loop.
+            %   propagate the satellite's attitude and angular velocity state.
+            %   It implements the coupled dynamics between the rigid body and the internal
+            %   angular momentum of the reaction wheels Assembly.
+            %
+            %   This is the primary public method that should be called from the main
+            %   simulation loop at each time step.
             %
             %   Syntax:
-            %       obj = obj.updateState(T_control, T_disturbance, dt)
+            %       obj = obj.updateState(Td, hw, hw_dot, dt)
             %
             %   Inputs:
-            %       obj           - The current instance of the Satellite object.
-            %       T_control     - The applied control torque (3x1) [Nm].
-            %       T_disturbance - The external disturbance torque (3x1) [Nm].
-            %       dt            - The integration time step [s].
+            %       obj    - The current Satellite object instance.
+            %       T_ext  - Vector (3x1) of net external torque (Disturbances)
+            %                applied TO the body [Nm]. (Corresponds to Td).
+            %       hw     - Vector (3x1) of net angular momentum of the reaction
+            %                wheels Assembly projected onto body axes [Nms].
+            %       hw_dot - Vector (3x1) of the time derivative of the wheels'
+            %                net angular momentum Assembly (dhw/dt) on body axes [Nm].
+            %                (Note: dhw/dt acts as the internal control torque,
+            %                being equal to -U_on_body by Newton's 3rd Law).
+            %       dt     - The integration time step [s].
             %
             %   Outputs:
-            %       obj - The Satellite object with its .State property updated.
+            %       obj - The updated Satellite object with its .State property modified.
+            %             (The state quaternion is automatically normalized).
 
             % Use the object's current state for the first RK4 step.
-            g1 = dt * obj.stateDerivative(T_control, T_disturbance, obj.State);
+            g1 = dt * obj.stateDerivative(T_ext, obj.State, hw, hw_dot);
             
             % Use temporary states for the intermediate RK4 steps.
-            g2 = dt * obj.stateDerivative(T_control, T_disturbance, obj.State + 0.5 * g1);
-            g3 = dt * obj.stateDerivative(T_control, T_disturbance, obj.State + 0.5 * g2);
-            g4 = dt * obj.stateDerivative(T_control, T_disturbance, obj.State + g3);
+            g2 = dt * obj.stateDerivative(T_ext, obj.State + 0.5 * g1, hw, hw_dot);
+            g3 = dt * obj.stateDerivative(T_ext, obj.State + 0.5 * g2, hw, hw_dot);
+            g4 = dt * obj.stateDerivative(T_ext, obj.State + g3, hw, hw_dot);
             
             % Update the object's actual state using the RK4 formula.
             obj.State = obj.State + (1/6) * (g1 + 2*g2 + 2*g3 + g4);
@@ -85,41 +96,94 @@ classdef Satellite
 
     methods (Access = private)
         % --- PRIVATE METHOD ---
-        function x_dot = stateDerivative(obj, U, Td, current_state)
+        function x_dot = stateDerivative(obj, T_ext, current_state, hw, hw_dot)
             % stateDerivative Calculates the state derivative [q_dot; w_dot].
             %
             %   This method implements the rotational kinematics and dynamics
-            %   equations for a rigid body. It is a private method because it
-            %   represents the internal logic of the satellite's model and does not
-            %   need to be accessed from outside the class.
+            %   equations for a rigid body considering internal angular
+            %   momentum from reaction wheels Assembly based on Path B logic
+            %   (using dhw/dt directly).
             %
             %   Inputs:
             %       obj           - The current instance of the Satellite object.
-            %       U             - Control torque [Nm].
-            %       Td            - Disturbance torque [Nm].
+            %       T_ext         - Net external disturbance torque [Nm].
             %       current_state - The state vector [q; w] to be evaluated.
+            %       hw            - Net angular momentum vector of reaction wheels Assembly
+            %                       projected onto body axes [Nms].
+            %       hw_dot        - Time derivative of the net angular momentum vector 
+            %                       of the wheels Assembly (dhw/dt) on body axes [Nm].
+            %                       (Note: dhw/dt = -U_on_body by Newton's 3rd Law).
             %
             %   Outputs:
             %       x_dot - The derivative of the state vector [q_dot; w_dot].
             
             % 1. Unpack the current state vector
             q = current_state(1:4);
-            w = current_state(5:7);
+            w = current_state(5:7); % Angular velocity of body w.r.t inertial frame
             
             % 2. Quaternion Kinematics
-            % Calculates the rate of change of the quaternion.
+            % Xi matrix for q_dot calculation (Standard convention)
             Xi = [-q(2), -q(3), -q(4);
                    q(1), -q(4),  q(3);
                    q(4),  q(1), -q(2);
                   -q(3),  q(2),  q(1)];
             q_dot = 0.5 * Xi * w;
             
-            % 3. Rotational Dynamics (Euler's Equation for a Rigid Body)
-            % Calculates the angular acceleration.
-            w_dot = obj.Inertia \ (Td + U - cross(w, obj.Inertia * w));
+            % 3. Rotational Dynamics (Coupled with Reaction Wheels - Path B)
+            % The governing equation is:
+            % I*w_dot = Td - hw_dot - w x (I*w + hw)
+            
+            % Calculate angular momentum of the satellite body
+            h_body = obj.Inertia * w;
+            
+            % Total angular momentum of the system (Body + Wheels)
+            h_total = h_body + hw;
+            
+            % Gyroscopic term (w x h_total)
+            gyroscopic_torque = cross(w, h_total);
+            
+            % Calculate angular acceleration w_dot (Corrected duplicity)
+            % Notice: 'U' has been removed from signature and equation.
+            w_dot = obj.Inertia \ (T_ext - hw_dot - gyroscopic_torque);
             
             % 4. Assemble the state derivative vector
             x_dot = [q_dot; w_dot];
         end
+        % % % function x_dot = stateDerivative(obj, U, Td, current_state)
+        % % %     % stateDerivative Calculates the state derivative [q_dot; w_dot].
+        % % %     %
+        % % %     %   This method implements the rotational kinematics and dynamics
+        % % %     %   equations for a rigid body. It is a private method because it
+        % % %     %   represents the internal logic of the satellite's model and does not
+        % % %     %   need to be accessed from outside the class.
+        % % %     %
+        % % %     %   Inputs:
+        % % %     %       obj           - The current instance of the Satellite object.
+        % % %     %       U             - Control torque [Nm].
+        % % %     %       Td            - Disturbance torque [Nm].
+        % % %     %       current_state - The state vector [q; w] to be evaluated.
+        % % %     %
+        % % %     %   Outputs:
+        % % %     %       x_dot - The derivative of the state vector [q_dot; w_dot].
+        % % % 
+        % % %     % 1. Unpack the current state vector
+        % % %     q = current_state(1:4);
+        % % %     w = current_state(5:7);
+        % % % 
+        % % %     % 2. Quaternion Kinematics
+        % % %     % Calculates the rate of change of the quaternion.
+        % % %     Xi = [-q(2), -q(3), -q(4);
+        % % %            q(1), -q(4),  q(3);
+        % % %            q(4),  q(1), -q(2);
+        % % %           -q(3),  q(2),  q(1)];
+        % % %     q_dot = 0.5 * Xi * w;
+        % % % 
+        % % %     % 3. Rotational Dynamics (Euler's Equation for a Rigid Body)
+        % % %     % Calculates the angular acceleration.
+        % % %     w_dot = obj.Inertia \ (Td + U - cross(w, obj.Inertia * w));
+        % % % 
+        % % %     % 4. Assemble the state derivative vector
+        % % %     x_dot = [q_dot; w_dot];
+        % % % end
     end
 end
